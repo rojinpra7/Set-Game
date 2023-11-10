aefadd9bff8ac212528204b2c215bb8bbb839ae7
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2023-11-10 15:14:35 -0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Rojin Prajapati on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
